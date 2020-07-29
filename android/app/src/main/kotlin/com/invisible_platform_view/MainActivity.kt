b5ce640d2e629ec1e898e9426b410c2fb0869f00
Andrei Diaconu <diaconu.andrei.tudor@gmail.com>
2020-07-29 18:51:15 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/invisible_platform_view/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.invisible_platform_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Webview',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: WebView(
+        initialUrl: "https://google.com",
+      ),
+    );
+  }
+}
